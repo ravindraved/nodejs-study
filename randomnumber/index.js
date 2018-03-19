2a9d12f41b981ad7cc4d3417c17e9aed9592d15c
Ravindra <rved76@gmail.com>
2018-03-19 09:57:44 -0700

    examples -  readline and eventloop

--- randomnumber/index.js
+++ randomnumber/index.js
@@ -3,9 +3,12 @@
 // Require the seedrandom module
 
 const seedrandom = require('seedrandom');
+console.log(seedrandom);
 
 //create a random number
 let rng = seedrandom();
+//Prints the function details
+console.log(rng);
 
 let myRandomNumber = Math.ceil((rng()*10));
 
