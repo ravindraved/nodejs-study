5cdd9101fa84beec83bfc233bb0e6393f79e01bc
Ravindra <ravindra.ved@lntinfotech.com>
2018-03-19 21:46:31 -0700

    intellij configs

--- helloyou/numbers.js
+++ helloyou/numbers.js
@@ -2,19 +2,28 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
-rl.question("Pick a number--", (number) => {
-    console.log(`you chose ${number}`);
+rl.question(
+    'Pick a number-->',
+    (number) =>
+    {
+        console.log(`you chose ${number}`);
 
+        rl.question(
+            'Pick a second number: ',
+            (number2) =>
+            {
+                const product = parseInt(number) +
+                    parseInt(number2);
+                console.log(`${number} + ${number2} = ${product}`);
+                rl.close();
 
-    rl.question("Pick a second number: ", (number2) => {
+            })
+;
 
-        const product = parseInt(number) + parseInt(number2);
-        console.log(`${number} + ${number2} = ${product}`);
-    });
-    rl.close();
+})
+;
 
-});
 
