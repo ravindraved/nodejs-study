84fb9329890b9d1b4ab89fd8f5fa0407e615fb3e
Ravindra <ravindra.ved@lntinfotech.com>
2018-03-22 03:52:53 -0700

    Writing and Deleting File .. First Interaction with CallBack Fun!!

--- files/index.js
+++ files/index.js
@@ -4,3 +4,10 @@ fs.writeFile('message.txt',"This is message in new file", (err) => {
     if(err) throw err;
     console.log("File has been saved!!");
 });
+
+fs.unlink('message.txt', (err) => {
+
+    if (err) throw err;
+    console.log(err + "Error in deleting file!!");
+})
+
