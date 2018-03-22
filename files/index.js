0964eefc1c813f0c543225f25cd1a9fb5196e213
Ravindra <ravindra.ved@lntinfotech.com>
2018-03-22 03:58:34 -0700

    Fix callback issue by nexting delete within create callback.

--- files/index.js
+++ files/index.js
@@ -3,11 +3,14 @@ const fs = require('fs');
 fs.writeFile('message.txt',"This is message in new file", (err) => {
     if(err) throw err;
     console.log("File has been saved!!");
+
+
+
+    fs.unlink('message.txt', (err) => {
+
+        if (err) throw err;
+        console.log(err + "->file deleted!!");
+    })
+
 });
 
-
-fs.unlink('message.txt', (err) => {
-
-    if (err) throw err;
-    console.log(err + "->file deleted!!");
-})
